df251ab1eb52919c39b7ae2617c9a1a6a55a4808
lancenonce <lancendavis47@gmail.com>
2024-09-05 03:44:44 +0300

    adding debugging to before_install.sh

--- cd-scripts/appspec-scripts/before_install.sh
+++ cd-scripts/appspec-scripts/before_install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
 
-APP_NAME=$(echo $APPLICATION_NAME | awk -F- '{ print $2 }')
+echo "Starting before_install.sh"
+echo "APPLICATION_NAME: $APPLICATION_NAME"
 
-if [ $APP_NAME = "nightly" ]; then
+APP_NAME=$(echo $APPLICATION_NAME | awk -F- '{ print $2 }')
+echo "APP_NAME: $APP_NAME"
+
+if [ "$APP_NAME" = "nightly" ]; then
+  echo "Creating directory for nightly build"
   if [ ! -d ~/$APP_NAME ]; then
     mkdir ~/$APP_NAME
   fi
 fi
 
+echo "before_install.sh completed successfully"
 exit 0
